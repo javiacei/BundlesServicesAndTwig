b5768e9035a0d795d20069c41cb4c7d8435cfaf8
Fco Javier Aceituno <fco.javier.aceituno@gmail.com>
2011-11-01 12:54:22 +0100

    Creando PHP Object Paginator

---
+++ src/JaviAcei/BookBundle/Paginator.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace JaviAcei\BookBundle;
+
+use 
+    Symfony\Component\HttpFoundation\Request
+;
+
+/**
+ * Paginator
+ *
+ * @package JaviAcei
+ * @subpackage Paginator
+ * @author Francisco Javier Aceituno <javier.aceituno@ideup.com>
+ */
+class Paginator 
+{
+
+    public $currentPage;
+    
+    public $itemsPerPage;
+   
+    public $maxPagerItems;
+    
+    public $totalItems;
+
+    /**
+     * @param Symfony\Component\HttpFoundation\Request $request
+     */
+    public function __construct(Request $request) 
+    {
+        // Página actual
+        $page = (int) $request->get('page');
+        $this->currentPage = ($page > 0) ? $page : $this->getFirstPage();
+
+        // Elementos por página
+        $itemsPerPage = (int) $request->query->get('limit');
+        $this->itemsPerPage = ($itemsPerPage > 0) ? $itemsPerPage : 10;
+        
+        // Número total de elementos
+        $this->totalItems = 0;
+    }
+
+    
+    /**
+     * Transforms the given Doctrine DQL into a paginated query
+     * If you need to paginate various queries in the same controller, you need to specify an $id
+     *
+     * @param array $collection
+     * @param string $id
+     * @return array
+     */
+    public function paginate(array $collection) 
+    {
+        $this->totalItems = count($collection);
+        $offset = ($this->currentPage - 1) * $this->itemsPerPage;
+
+        return array_slice($collection, $offset, $this->itemsPerPage);
+    }
+
+   
+
+    /**
+     * Get the next page number
+     *
+     * @return int
+     */
+    public function getNextPage() 
+    {
+        return $this->currentPage + 1;
+    }
+
+    /**
+     * Get the previous page number
+     * 
+     * @return int
+     */
+    public function getPreviousPage() 
+    {
+        return $this->currentPage + 1;
+    }
+
+    /**
+     * Gets the last page number
+     *
+     * @param string $id
+     * @return int
+     */
+    public function getLastPage() 
+    {
+        return (int) ceil($this->totalItems / $this->itemsPerPage);
+    }
+
+    /**
+     * @return int
+     */
+    public function getFirstPage() 
+    {
+        return 1;
+    }
+
+}
